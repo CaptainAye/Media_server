08f7ea3321499c27831a328bbafb8f619c419015
Tomasz Letachowicz <tomasz.letachowicz@gmail.com>
2016-11-16 20:20:42 +0100

    Obsluga rysowania i listy pieter

---
+++ lib/Common/src/org/mediaserver/exceptions/KeyNotFoundException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mediaserver.exceptions;
+
+/**
+ *
+ * @author Tomek
+ */
+public class KeyNotFoundException extends Exception{
+    public KeyNotFoundException(){
+        super();
+    }
+    
+    
+}
